60b55afc964ce9e9cf18b5eab73e00325dc84882
Andrea Tettamanti <116306022+IlTetta@users.noreply.github.com>
2025-07-29 12:03:14 +0200

    Creazione server con API

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const db = require('./config/db');
+
+const userRoutes = require('./routes/userRoutes');
+
+dotenv.config(); // Load environment variables
+
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use('/api/users', userRoutes);
+
+// Rotta di prova
+app.get('/', (req, res) => {
+    res.send('Benvenuto in CoWorkSpace API!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server in ascolto sulla porta ${PORT}`);
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.statusCode || 500).json({
+        message: err.message || 'Errore interno del server',
+        error: process.env.NODE_ENV === 'development' ? err : {}
+    });
+});
