52e6829a48935fa621b2c1ae78b95761317dd7a0
Andrea Tettamanti <116306022+IlTetta@users.noreply.github.com>
2025-08-03 22:03:49 +0200

    fix test

--- src/backend/middleware/auth.js
+++ src/backend/middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const pool = require('../config/db');
 const catchAsync = require('../utils/catchAsync');
 
-const cathAsync = fn => (req, res, next) => {
+const catchAsync = fn => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
 };
 
