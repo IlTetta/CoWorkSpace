dfa63091ecf137e64cd144ee7effdff4d4d631d3
Andrea Tettamanti <116306022+IlTetta@users.noreply.github.com>
2025-08-03 15:06:43 +0200

    fix db.js

--- src/backend/config/db.js
+++ src/backend/config/db.js
@@ -9,15 +9,4 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
-// Test connessione
-// pool.query('SELECT NOW()', (err, res) => {
-//   if (err) {
-//     console.error('Errore di connessione al database:', err.stack);
-//     console.log("DB_USER:", process.env.DB_USER);
-//     console.log("DB_PASSWORD:", typeof process.env.DB_PASSWORD);
-//   } else {
-//     console.log('Connessione al database avvenuta con successo:', res.rows[0].now);
-//   }
-// });
-
 module.exports = pool;
