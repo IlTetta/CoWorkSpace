707b81461e61b43e4817d9848403556c77709bfb
Andrea Tettamanti <116306022+IlTetta@users.noreply.github.com>
2025-08-07 17:26:41 +0200

    index con modelli globali

---
+++ src/backend/models/index.js
@@ -0,0 +1,13 @@
+/**
+ * Index file per esportare tutti i modelli
+ * Centralizza l'accesso ai modelli per il resto dell'applicazione
+ */
+
+const SpaceType = require('./SpaceType');
+
+module.exports = {
+    SpaceType
+};
+
+// Alternativa per import singoli:
+// module.exports.SpaceType = require('./SpaceType');
