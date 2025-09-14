9b29333352d7c6e2a93ec8659e98c16322408324
Andrea Tettamanti <116306022+IlTetta@users.noreply.github.com>
2025-09-14 17:05:12 +0200

    fix firebase.js

--- src/backend/config/firebase.js
+++ src/backend/config/firebase.js
@@ -1,10 +1,10 @@
 const admin = require('firebase-admin');
+const path = require('path');
 
-// Legge il JSON dalla variabile d'ambiente su Render
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_PATH || path.join(__dirname, "../../firebase-service-account.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccountPath)
 });
 
 module.exports = admin;
